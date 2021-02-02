410c709e305e195bbd2a4add159701d3586fe5cf
Syeda Naima Hassan <naima.hassan434@gmail.com>
2021-02-02 20:37:27 +0600

    Object create

--- index.js
+++ index.js
@@ -4,3 +4,21 @@ console.log("My name is " + Name);
 console.log(1 + 1);
 let message = "Hi,\n How are you?";
 console.log(message);
+
+// Exercise:Person Object
+// Properties: name, age, isMarried, homeAddress, friends
+
+let person = {
+  name: 'john',
+  age: 49,
+  isMarried: true,
+  homeAddress: {
+    long: 83.33,
+    lat: 24.44,
+  },
+  friends: ['jeff', 'merry','john']
+};
+// console.log(person);
+console.log(person.age);
+console.log(person.homeAddress.lat);
+console.log(person.friends[1]);
