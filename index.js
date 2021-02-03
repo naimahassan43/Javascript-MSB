1f9518941eb12e1a2e0c9f5f3e57dd9cda7a0a6d
Syeda Naima Hassan <naima.hassan434@gmail.com>
2021-02-04 00:57:00 +0600

    Grading system-Exercise

--- index.js
+++ index.js
@@ -153,3 +153,32 @@ if (year % 400 === 0) {
 } else {
   console.log("This Year is not a leap year");
 }
+
+//Problem: Our Grading System 
+//Take a Mark
+//if the mark is in between 80-100,print "A+"
+//if the mark is in between 70-79,print "A"
+//if the mark is in between 60-69,print "A-"
+//if the mark is in between 50-59,print "B"
+//if the mark is in between 40-49,print "C"
+//if the mark is in between 33-39,print "D"
+//point out the invalid marks
+
+let mark = 130;
+if(mark<0 || mark>100){
+  console.log('Invalid Mark!!')
+}else if (mark >= 80 && mark <= 100) {
+  console.log('A+');
+}else if (mark >= 70 && mark <= 79) {
+  console.log('A');
+}else if (mark >= 60 && mark <= 69) {
+  console.log('A-');
+}else if (mark >= 50 && mark <= 59) {
+  console.log('B');
+}else if (mark >= 40 && mark <= 49) {
+  console.log('C');
+}else if (mark >= 33 && mark <= 39) {
+  console.log('D');
+} else {
+  console.log('F')
+}
