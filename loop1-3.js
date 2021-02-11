dd475a3e18ce3f3de677cb1183a48db90db220c5
Syeda Naima Hassan <naima.hassan434@gmail.com>
2021-02-11 21:51:58 +0600

    Loop Assignment:1-3

---
+++ loop1-3.js
@@ -0,0 +1,19 @@
+//No.1 1 to 100 print
+
+for (let ass1 = 1; ass1 <= 100; ass1++) {
+  console.log(ass1);
+}
+//No.2 1 to 100 print Reversal
+
+for (let ass2 = 100; ass2 >= 1; ass2--) {
+  console.log(ass2);
+}
+//No.3 Sum and Average
+
+let sum = 0;
+for (let ass3 = 1; ass3 <= 100; ass3++) {
+  sum += ass3;
+}
+console.log(sum);
+let avg = sum / 100;
+console.log(avg);
