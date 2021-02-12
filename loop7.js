1b2a4ee20f477e96dbe5b289801a32e0825b828b
Syeda Naima Hassan <naima.hassan434@gmail.com>
2021-02-12 12:35:31 +0600

    Loop Assignment:7

---
+++ loop7.js
@@ -0,0 +1,7 @@
+let number = 20;
+
+for (let multiplier = 1; multiplier <= 10; multiplier++){
+  let product = (number * multiplier);
+    console.log (number + ' x ' + multiplier + ' = '+ product);
+  
+}
