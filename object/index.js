ae05b7bc39903db711ed90b3df589c3ac14f7318
Syeda Naima Hassan <naima.hassan434@gmail.com>
2021-04-07 23:01:48 +0100

    Traversing Object Entries

--- object/index.js
+++ object/index.js
@@ -1,4 +1,4 @@
-//node object/index.js
+
 const user = {
   name: 'john',
   age: 47,
@@ -9,23 +9,23 @@ console.log(user);
 //Property Access in Object with Dot 
 console.log(user.hobby);
 //Property Access in Object with Bracket
-console.log(user['age']);
+// console.log(user['age']);
 
 /*Object Manupulation: Adding, Modifying, Deleting*/
 
 //Add 
-// user.email = 'john@gmail.com';
-// user['phone'] = '01911111';
+user.email = 'john@gmail.com';
+user['phone'] = '01911111';
 
 //Update
-// user.isMarried = false;
-// user.age = user.age + 3;
+user.isMarried = false;
+user.age = user.age + 3;
 
 //Delete
-// delete user.email;
-// console.log(user);
+delete user.email;
+console.log(user);
 
-/*************/
+/****** Object Methods & this keyword *******/
 const user1 = {
   name: 'john',
   age: 55,
@@ -37,3 +37,23 @@ const user1 = {
   }
 };
 user1.calculateAge();
+/******Traversing Object Entries *******/
+const user2 = {
+  name: 'john',
+  age: 47,
+  gender: 'male',
+  email: 'john@gmail.com',
+  phone:'01911111'
+};
+//==========Way1
+for (let key in user2) {
+  console.log(key, user2[key]);
+}
+//============Way2
+console.log(Object.keys(user2));
+console.log(Object.values(user2));
+//============Way3
+for (let val of Object.values(user2)) {
+  console.log(val);
+}
+// node object/index.js
