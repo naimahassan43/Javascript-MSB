620df37daa493fe5d15937885f27ab675dd5cad0
Syeda Naima Hassan <naima.hassan434@gmail.com>
2021-04-08 17:54:44 +0100

    Function Basics

---
+++ function/index.js
@@ -0,0 +1,18 @@
+//Function Declaration
+
+function aboutMe(name,age) {
+  const message = `I am ${name} and I am ${age} years old.`;
+  console.log(message);
+}
+//Function Call
+aboutMe('John', 20);
+
+function calSum(num1, num2) {
+  let sum = num1 + num2;
+  return sum;
+}
+let result = calSum(19, 30);
+console.log(result);
+// node function/index.js
+
+
