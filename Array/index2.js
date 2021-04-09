df9e19aa8616bef5d94be071195212ad3b2a1304
Syeda Naima Hassan <naima.hassan434@gmail.com>
2021-04-09 17:42:14 +0100

    Combining and Slicing

--- Array/index2.js
+++ Array/index2.js
@@ -17,4 +17,16 @@ const alleven = even.some(function (num) {
 });
 
 console.log(alleven);
+
+/******Array: concat, slice *********/
+const num1 = [1, 2, 3];
+const num2 = [4, 5, 6];
+
+const num = num1.concat(num2);
+console.log(num);
+
+const numbs = [1, 2, 3, 4, 5, 6,7,8];
+// const sliceArray = numbs.slice(2,5);
+const sliceArray = numbs.slice();
+console.log(sliceArray);
 // node Array/index2.js
