8f5094f48f59d2f8de13970eccb33d8a55722a2b
Syeda Naima Hassan <naima.hassan434@gmail.com>
2021-04-09 01:29:04 +0100

    Finding element in Array

--- Array/index.js
+++ Array/index.js
@@ -23,4 +23,39 @@ const numb = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 numb.splice(2, 2);
 console.log(numb);
+
+
+/***************** Finding an element(Primitive type) ************/
+
+const numbrs = [1, 2, 3, 2, 5, 2, 7];
+
+console.log(numbrs.includes(3));
+console.log(numbrs.includes(3, 3));
+console.log(numbrs.indexOf(5));
+console.log(numbrs.lastIndexOf(2));
+/***************** Finding an element(Reference data type) ************/
+
+//finding object in Array
+
+const doctors = [
+  { name:'john', age:28},
+  { name:'mary', age:32},
+  { name:'smith', age:29},
+];
+
+// const result = doctors.find(function (doctor) {
+//   // return doctor.name === 'mary';
+//   return doctor.age>30;
+// });
+
+// const result = doctors.findIndex(function (doctor) {
+//   return doctor.name === 'mary';
+  
+// });
+
+// const result = doctors.findIndex( (doctor) =>
+//    doctor.name === 'mary');
+// console.log(result);
+console.log(doctors.findIndex( (doctor) =>
+   doctor.name === 'smith'));
 // node Array/index.js
