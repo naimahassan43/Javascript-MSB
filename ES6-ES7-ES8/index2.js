5057fdfeeb6a141ef39222fdc16aaf4b7f5627b1
Syeda Naima Hassan <naima.hassan434@gmail.com>
2021-04-13 13:31:11 +0100

    ES6 Module (Import & Export)

---
+++ ES6-ES7-ES8/index2.js
@@ -0,0 +1,8 @@
+//ES6 Module
+
+import {addition,sub} from './math.js';
+
+console.log(addition(50,30));
+console.log(sub(80,30));
+
+// node ES6-ES7-ES8/index2.js
---
+++ ES6-ES7-ES8/math.js
@@ -0,0 +1,8 @@
+// const addition = ((num1, num2) => num1 + num2);
+
+// export default addition;
+
+//Multiple export 
+
+export const addition = (num1, num2) => num1 + num2;
+export const sub = (num1, num2) => num1 - num2;
